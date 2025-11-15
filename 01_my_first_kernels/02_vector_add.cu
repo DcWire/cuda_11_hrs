7c582daca4b4397abf1215bdad5c30b337b36403
AnasAli986 <60493038+DcWire@users.noreply.github.com>
2025-11-15 17:05:23 +0530

    Complete 02_vector_add for 3d computation of vector addition

--- 01_my_first_kernels/02_vector_add.cu
+++ 01_my_first_kernels/02_vector_add.cu
@@ -4,7 +4,11 @@
 #include<cuda_runtime.h>
 
 #define N 10000000 // 10 million vector size
-#define BLOCK_SIZE 256
+// 1D Block Size
+#define BLOCK_SIZE 1024
+#define BLOCK_SIZE_3D_X 16
+#define BLOCK_SIZE_3D_Y 8
+#define BLOCK_SIZE_3D_Z 8
 
 // CPU vector addition 
 void vector_add_cpu(float *a, float *b, float *c, int n) {
@@ -14,13 +18,27 @@ void vector_add_cpu(float *a, float *b, float *c, int n) {
 
 }
 
+// 1D vector addition
 // CUDA kernel for vector addition 
 __global__ void vector_add_gpu(float *a, float *b, float *c, int n) {
     // blockIdx.x and not y because the block has only an x dimension
-    int threadIdx = blockIdx.x * blockDim.x + blockIdx.x; 
+    int i = blockIdx.x * blockDim.x + threadIdx.x; 
 
-    if (threadIdx < n) {
-        c[threadIdx] = a[threadIdx] + b[threadIdx];
+    if (i < n) {
+        c[i] = a[i] + b[i];
+    }
+}
+
+__global__ void vector_add_gpu_3d(float *a, float *b, float *c, int nx, int ny, int nz) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    int k = blockIdx.z * blockDim.z + threadIdx.z;
+
+    if (i < nx && j < ny && k < nz) {
+        int idx = i + j * nx + k * nx * ny;
+        if (idx < (nx * ny * nz)) {
+            c[idx] = a[idx] + b[idx];
+        }
     }
 }
 
@@ -38,13 +56,14 @@ double get_time() {
 }
 
 int main(void) {
-    float *h_a, *h_b, *h_c_cpu, *h_c_gpu;
+    float *h_a, *h_b, *h_c_cpu, *h_c_gpu, *h_c_gpu_3d;
     float *d_a, *d_b, *d_c;
-    size_t size = size
+    size_t size = N * sizeof(float);
     h_a = (float *) malloc(size);
     h_b = (float *) malloc(size);
     h_c_cpu = (float *) malloc(size);
     h_c_gpu = (float *) malloc(size);
+    h_c_gpu_3d = (float *) malloc(size);
     
     // Initialize vectors
     srand(time(NULL));
@@ -57,15 +76,25 @@ int main(void) {
     cudaMalloc(&d_b, size);
     cudaMalloc(&d_c, size);
 
-    cudaMemcpy(d_a, h_a, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, h_b, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_a, h_a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, size, cudaMemcpyHostToDevice);
 
     int num_blocks = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    // Need to define the grid and block dimensions for 3d 
+    int nx = 100, ny = 100, nz = 1000; // N = nx * ny * nz
+    dim3 block_size_3d(BLOCK_SIZE_3D_X, BLOCK_SIZE_3D_Y, BLOCK_SIZE_3D_Z);
+    dim3 num_blocks_3d(
+            (nx + block_size_3d.x - 1) / block_size_3d.x, 
+            (ny + block_size_3d.y - 1) / block_size_3d.y,
+            (nz + block_size_3d.z - 1) / block_size_3d.z
+    );
+
 
     printf("Performing warmup runs for cahce...\n");
     for (int i=0; i<3; i++) {
         vector_add_cpu(h_a, h_b, h_c_cpu, N);
         vector_add_gpu<<<num_blocks, BLOCK_SIZE>>>(d_a, d_b, d_c, N);
+        vector_add_gpu_3d<<<num_blocks_3d, block_size_3d>>>(d_a, d_b, d_c, nx, ny, nz);
         cudaDeviceSynchronize();
     }
     
@@ -81,7 +110,7 @@ int main(void) {
 
     double cpu_avg_time = cpu_total_time / (float)runs; 
 
-    printf("Benchmarking GPU implementation...\n");
+    printf("Benchmarking 1D GPU implementation...\n");
     double gpu_total_time = 0.0;
     for(int i=0; i<runs; i++) {
         double start_time = get_time();
@@ -93,16 +122,41 @@ int main(void) {
 
     double gpu_avg_time = gpu_total_time / (float)runs; 
     
+    printf("Benchmarking 3D GPU implementation...\n"); 
+    
+    double gpu_3d_total_time = 0.0;
+    for(int i=0; i<runs; i++) {
+        double start_time = get_time(); 
+        vector_add_gpu_3d<<<num_blocks_3d, block_size_3d>>>(d_a, d_b, d_c, nx, ny, nz);
+        cudaDeviceSynchronize();
+        double end_time = get_time();
+        gpu_3d_total_time += end_time - start_time;
+    }
+    
+    double gpu_avg_3d_time = gpu_3d_total_time / (float)runs;
+    
     printf("CPU average time: %f milliseconds\n", cpu_avg_time*1000);
     printf("GPU average time: %f milliseconds\n", gpu_avg_time*1000);
-    printf("Speedup: %fx\n", cpu_avg_time / gpu_avg_time);
+    printf("GPU 3D AVG TIME: %f milliseonds \n", gpu_avg_3d_time*1000);
+    printf("Speedup 1D: %fx\n", cpu_avg_time / gpu_avg_time);
+    printf("Speedup 3D: %fx\n", cpu_avg_time / gpu_avg_3d_time);
 
-    cudaMemcpy(h_c_gpu, d_c, cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_c_gpu, d_c, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_c_gpu_3d, d_c, size, cudaMemcpyDeviceToHost);
+
     bool correct = true; 
     for(int i=0; i<N; i++) {
         // Check against a threshold value
         if(fabs(h_c_cpu[i] - h_c_gpu[i]) > 1e-5) {
-            corrent = false;
+            correct = false;
+            break;
+        }
+    }
+
+    for(int i=0; i<N; i++) {
+        // Check against a threshold value
+        if(fabs(h_c_cpu[i] - h_c_gpu_3d[i]) > 1e-5) {
+            correct = false;
             break;
         }
     }
